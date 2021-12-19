1c26a754cd42accd1d1c3a2c99d458500324ff9a
Omar Tayfour <omartayfourr@gmail.com>
2021-12-19 16:47:47 +0200

    regex

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-    validates :mobile , uniqueness: true
-    validates :name, presence: true
-    validates :address, presence: true
+  validates :mobile, uniqueness: true, presence: true, format: { with: /(010|011|012|015)(\d{8})/ }
+  validates :name, presence: true
+  validates :address, presence: true
 end
